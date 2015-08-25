5c9a72b6d6952d6c90ebf293588ce0e61e05cc2e
Simon Raes <simonraes@gmail.com>
2015-08-25 22:11:56 +0200

    Testing routes

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use('/', routes);
+/*
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
